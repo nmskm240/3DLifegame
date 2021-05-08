cc30349c91bae41fd44d766526e1ac1b5eb5524c
Tatsuya <ufufu2929@gmail.com>
2021-05-08 21:13:21 +0900

    test

---
+++ Assets/Scripts/Lifegame/Rules/Model3Da.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Lifegame;
+
+namespace Lifegame.Rules
+{
+    public class Model3D01 : IRule
+    {
+        public bool NextAlive(Cell cell)
+        {
+            var count = 0;
+            foreach (var aroundCell in cell.AroundCells)
+            {
+                count += (aroundCell.IsAlive) ? 1 : 0;
+            }
+            if(cell.IsAlive)
+            {
+                return (count % 2 == 0);
+            }
+            else
+            {
+                return (count % 2 == 1);
+            }
+        }
+    }
+}
