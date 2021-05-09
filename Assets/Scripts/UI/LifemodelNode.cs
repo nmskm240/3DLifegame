5f4ebf8a504d7c31cba27da7e51feb2d74c84d15
nmskm240 <bokaronogod@gmail.com>
2021-05-09 12:36:06 +0900

    LifeModelまわりの動作の改善

--- Assets/Scripts/UI/LifemodelNode.cs
+++ Assets/Scripts/UI/LifemodelNode.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using TMPro;
 using Network;
+using Network.RequestDto;
+using Network.ResponseDto;
 
 namespace UI
 {    
@@ -13,15 +16,41 @@ namespace UI
         [SerializeField]
         private TextMeshProUGUI _name;
         [SerializeField]
+        private TextMeshProUGUI _creatorInfo;
+        [SerializeField]
+        private TextMeshProUGUI _favoriteInfo;
+        [SerializeField]
+        private Button _favorite;
+        [SerializeField]
         private LoadStageData _out;
 
         private LifeModel _model;
+
+        private void Awake() 
+        {
+            _favorite.onClick.AddListener(() => 
+            {
+                var url = NetworkManager.Instance.GetMethod(MethodType.PostModelFavorite);
+                var request = new FavoriteLifeModelRequestDto()
+                {
+                    id = _model.id,
+                };
+                StartCoroutine(NetworkManager.Instance.WebRequest.Post<FavoriteLifeModelRequestDto, NoneResponseDto>(url, request, x => {  }, error =>
+                {
+                    var factory = new DialogFactory();
+                    var dialog = factory.Create().GetComponent<Dialog>();
+                    dialog.Show(DialogType.Error, error.Contains("500") ? "Already have favorited" : error);
+                }, true));
+            });
+        }
 
         public void Init(int rank, LifeModel model)
         {
             _model = model;
             _rank.text = rank.ToString();
             _name.text = model.name;
+            _creatorInfo.text = model.user.name;
+            _favoriteInfo.text = model.favorite.ToString();
         }
 
         public void Init(LifeModel model)
@@ -29,12 +58,19 @@ namespace UI
             _model = model;
             _rank.text = "";
             _name.text = model.name;
+            _creatorInfo.text = model.user.name;
+            _favoriteInfo.text = model.favorite.ToString();
         }
 
         public void OnPointerClick(PointerEventData e)
         {
-            _out.Copy(_model);
-            SceneManager.LoadScene("LifeModelInfo");
+            var factory = new DialogFactory();
+            var dialog = factory.Create().GetComponent<Dialog>();
+            dialog.Show(DialogType.Switch, _model.name + " play?", agree => 
+            {
+                _out.Copy(_model);
+                SceneManager.LoadScene("Game");
+            });
         }
     }
 }
